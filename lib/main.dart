0124580ba44a1c2a949e1bec31dc4cf67f2d8630
malhaq <mohammadabedalhaq02@gmail.com>
2024-12-08 01:45:26 +0200

    current location button working & new api call to google maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:places_app/screens/place.dart';
@@ -35,7 +36,8 @@ final theme = ThemeData().copyWith(
   ),
 );
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   runApp(
     const ProviderScope(
       child: MyApp(),
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:places_app/model/place.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -10,26 +16,94 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+  PlaceLocation? _pickedLocation;
+  var _gettingLocation = false;
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+      _gettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+
+    if(lat == null||lng == null){
+      return;
+    }
+
+    final url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=${dotenv.env['GOOGLE_MAPS_API_KEY']}');
+    final res = await http.get(url);
+    final resData = json.decode(res.body);
+    final address = resData['results'][0]['formatted_address'];
+    setState(() {
+      _pickedLocation = PlaceLocation(address: address, latitude: lat, longitude: lng);
+      _gettingLocation = false;
+    });
+    
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = Text(
+      'No Location Chosen',
+      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+      textAlign: TextAlign.center,
+    );
+    if (_gettingLocation) {
+      content = const CircularProgressIndicator();
+    }
+
     return Column(
       children: [
-        Container(),
+        Container(
+          alignment: Alignment.center,
+          width: double.infinity,
+          height: 170,
+          decoration: BoxDecoration(
+            border: Border.all(
+                width: 1,
+                color: Theme.of(context).colorScheme.primary.withOpacity(0.5)),
+          ),
+          child: content,
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton.icon(
               icon: const Icon(Icons.location_on),
-              onPressed: () {
-
-              },
+              onPressed: _getCurrentLocation,
               label: const Text('Get current location'),
             ),
             TextButton.icon(
               icon: const Icon(Icons.map_outlined),
-              onPressed: () {
-                
-              },
+              onPressed: () {},
               label: const Text('Select on Map'),
             ),
           ],
